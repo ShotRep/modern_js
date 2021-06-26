97528f3cafffe45f9c8fe9408c7656ca547151e1
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-06-26 15:54:29 -0500

    incremental commit

--- JavaScript/Scope_HOFunctions_CallBacks.js
+++ JavaScript/Scope_HOFunctions_CallBacks.js
@@ -66,30 +66,94 @@
 //HOFunctions and how they work.
 //Using functions within functions. Excepting functions as arguments and returning functions as values.
 
-//First functions are objects.
-function add(x, y) {
-  return x + y;
-}
-const subtract = function (x, y) {
-  return x - y;
-};
-function multiply(x, y) {
-  return x * y;
-}
-const divide = function (x, y) {
-  return x / y;
-};
+// //First functions are objects.
+// function add(x, y) {
+//   return x + y;
+// }
+// const subtract = function (x, y) {
+//   return x - y;
+// };
+// function multiply(x, y) {
+//   return x * y;
+// }
+// const divide = function (x, y) {
+//   return x / y;
+// };
 
-const operations = [add, subtract, multiply, divide];
+// const operations = [add, subtract, multiply, divide];
 
-// console.log(operations[0](5, 6));
-//since functions are values we can just use them as such
-for (let operator of operations) {
-  let result = operator(5, 6); //<=parens execute the function
-  console.log(result);
+// // console.log(operations[0](5, 6));
+// //since functions are values we can just use them as such
+// for (let operator of operations) {
+//   let result = operator(5, 6); //<=parens execute the function
+//   console.log(result);
+// }
+
+// const thing = {
+//   doSomething: multiply, //<=storing a function as a value
+// };
+// console.log(thing.doSomething(7, 7)); //<=object.property.parens = method
+
+//HOFunctions
+// 1.) Functions as arguments
+// function frankGore() {
+//   console.log("reasons");
+// }
+
+// function repeat(action, num) {
+//   for (let i = 0; i < num; i++) {
+//     action();
+//   }
+// }
+// repeat(frankGore, 21); //<= function as an argument
+
+//HOFunctions
+//2.) Functions as return arguments. returning a function from within a function.
+// function multiplyBy(num) {
+//   return function (x) {
+//     return x * num;
+//   };
+// }
+// const triple = multiplyBy(3);
+// const double = multiplyBy(2);
+// const halve = multiplyBy(0.5);
+
+// console.log(triple(5), double(5), halve(5));
+
+//////CALLBACK FUNCTIONS
+//a callback function is a function passed into another function as
+//an argument, which is then invoked inside the outer function.
+
+//we have been doing this already.
+//intro to the subject that is very very common
+
+// function callTwice(func) {
+//   func();
+//   func();
+// }
+
+// function laugh() {
+//   console.log("HaHaHaaa");
+// }
+// callTwice(laugh); //<=Callback Function
+
+// function grumpus() {
+//   console.log("Katherine is a grumpus");
+// }
+// setTimeout(grumpus, 1000);
+// //same just a dif way of writing it.
+// setTimeout(function () {
+//   console.log("Katherine is a major grumpus!");
+// }, 2000);
+
+function grumpus() {
+  console.log("Katherine is a grumpus");
 }
+// const btn = document.querySelector("button");
+// btn.addEventListener("click", grumpus);
 
-const thing = {
-  doSomething: multiply, //<=storing a function as a value
-};
-console.log(thing.doSomething(7, 7)); //<=object.property.parens = method
+const btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+  //<=anonymous function instead
+  alert("I said don't click me!");
+});
