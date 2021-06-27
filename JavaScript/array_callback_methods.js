d9fc15f19018cadfc2b2061cda7658dff62a12ef
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-06-27 11:41:23 -0500

    starting new JS topic some and every

--- JavaScript/array_callback_methods.js
+++ JavaScript/array_callback_methods.js
@@ -81,8 +81,8 @@ every
 // console.log(texts);
 // console.log(caps);
 
-const numbers = [17, 15, 13, 11, 9, 7, 5, 3, 2];
-const words = ["asap", "byob", "rsvp", "diy"];
+// const numbers = [17, 15, 13, 11, 9, 7, 5, 3, 2];
+// const words = ["asap", "byob", "rsvp", "diy"];
 
 // const newArray = numbers.map(function (num) {
 //   return num * 2; //<=you must return, this pushes into new array for us
@@ -102,30 +102,133 @@ const words = ["asap", "byob", "rsvp", "diy"];
 // });
 // console.log(newArray);
 
+// const books = [
+//   {
+//     title: "Good Omens",
+//     authors: ["Terry Prachett", "Neil Gaiman"],
+//     rating: 4.25,
+//   },
+//   {
+//     title: "Bone: The Complete Edition",
+//     authors: ["Jeff Smith"],
+//     rating: 4.42,
+//   },
+//   {
+//     title: "American Gods",
+//     authors: ["Jeff Smith"],
+//     rating: 4.11,
+//   },
+//   {
+//     title: "A Gentleman in Moscow",
+//     authors: ["Neil Gaiman"],
+//     rating: 4.36,
+//   },
+// ];
+
+// const newArray = books.map(function (book) {
+//   return book.title.toUpperCase();
+// });
+// console.log(newArray);
+
+//////FIND
+//returns the value of the first element in the array that satisfies the provided testing function and stops.  So 1 result which is the first found.
+
+let movies = [
+  "The Fantastic Mr. Fox",
+  "Mr. and Mrs. Smith",
+  "Mrs. Doubtfire",
+  "Mr. Deeds",
+];
+let movie = movies.find((movie) => {
+  return movie.includes("Mrs");
+});
+console.log(movie);
+
+let movie2 = movies.find((movie) => movie.indexOf("Mrs") === 0);
+console.log(movie2);
+
+/////FILTER///
+//creates a new array with all elements that pass the test implemented by the provided function.
+
+const nums = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const odds = nums.filter((n) => {
+  return n % 2 === 1;
+});
+console.log(odds);
+
+const smallNums = nums.filter((n) => n <= 5);
+console.log(smallNums);
+
+//using filter with includes
+
 const books = [
   {
     title: "Good Omens",
-    authors: ["Terry Prachett", "Neil Gaiman"],
+    authors: ["Terry Pratchett", "Neil Gaiman"],
     rating: 4.25,
+    genres: ["fiction", "fantasy"],
+  },
+  {
+    title: "Changing My Mind",
+    authors: ["Zadie Smith"],
+    rating: 3.83,
+    genres: ["nonfiction", "essays"],
   },
   {
     title: "Bone: The Complete Edition",
     authors: ["Jeff Smith"],
     rating: 4.42,
+    genres: ["fiction", "graphic novel", "fantasy"],
   },
   {
     title: "American Gods",
-    authors: ["Jeff Smith"],
+    authors: ["Neil Gaiman"],
     rating: 4.11,
+    genres: ["fiction", "fantasy"],
   },
   {
     title: "A Gentleman in Moscow",
-    authors: ["Neil Gaiman"],
+    authors: ["Amor Towles"],
     rating: 4.36,
+    genres: ["fiction", "historical fiction"],
+  },
+  {
+    title: "The Name of the Wind",
+    authors: ["Patrick Rothfuss"],
+    rating: 4.54,
+    genres: ["fiction", "fantasy"],
+  },
+  {
+    title: "The Overstory",
+    authors: ["Richard Powers"],
+    rating: 4.19,
+    genres: ["fiction", "short stories"],
+  },
+  {
+    title: "The Way of Kings",
+    authors: ["Brandon Sanderson"],
+    rating: 4.65,
+    genres: ["fantasy", "epic"],
+  },
+  {
+    title: "Lord of the flies",
+    authors: ["William Golding"],
+    rating: 3.67,
+    genres: ["fiction"],
   },
 ];
 
-const newArray = books.map(function (book) {
-  return book.title.toUpperCase();
-});
+const newArray = books.filter((b) => b.genres.includes("fantasy"));
 console.log(newArray);
+
+const short = books.filter(
+  (b) => b.genres.includes("short stories") || b.genres.includes("essays")
+);
+console.log(short);
+
+const query = "The";
+const results = books.filter((book) => {
+  const title = book.title.toLocaleLowerCase();
+  return title.includes(query.toLowerCase());
+});
+console.log(results);
--- JavaScript/arrow_functions.js
+++ JavaScript/arrow_functions.js
@@ -28,4 +28,32 @@ const isEven = (num) => {
 };
 console.log(isEven(3));
 
-/////////////////IMPLICIT RETURNS//
+/////////////////IMPLICIT RETURNS////////////////////
+//losing the "return"
+/*
+const isEven = function (num) {    <= function expression
+  return num % 2 === 0;            
+};
+const isEven = (num) => {         <= arrow function with parens
+  return num % 2 === 0;
+};
+const isEven = num => {          <= arrow function without parens
+  return num % 2 === 0;
+};
+const isEven = num => (num % 2 === 0);   <= IMPLICIT RETURN
+const isEven = num => num % 2 === 0;     <= 1 Liner IMPLICIT RETURN
+*/
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers = nums.map(function (n) {
+  return n * 2;
+});
+console.log(numbers);
+
+const numbers2 = nums.map((n) => {
+  return n * 2;
+});
+console.log(numbers2);
+
+const numbers3 = nums.map((n) => n * 2);
+console.log(numbers3);
