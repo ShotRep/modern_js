b27d977325609ea6587f86e97d4628bf1bb020f9
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-06-28 06:55:52 -0500

    adding of comments regarding .slice, .splice, and .map

--- JavaScript/array_callback_methods.js
+++ JavaScript/array_callback_methods.js
@@ -133,102 +133,194 @@ every
 //////FIND
 //returns the value of the first element in the array that satisfies the provided testing function and stops.  So 1 result which is the first found.
 
-let movies = [
-  "The Fantastic Mr. Fox",
-  "Mr. and Mrs. Smith",
-  "Mrs. Doubtfire",
-  "Mr. Deeds",
-];
-let movie = movies.find((movie) => {
-  return movie.includes("Mrs");
-});
-console.log(movie);
+// let movies = [
+//   "The Fantastic Mr. Fox",
+//   "Mr. and Mrs. Smith",
+//   "Mrs. Doubtfire",
+//   "Mr. Deeds",
+// ];
+// let movie = movies.find((movie) => {
+//   return movie.includes("Mrs");
+// });
+// console.log(movie);
 
-let movie2 = movies.find((movie) => movie.indexOf("Mrs") === 0);
-console.log(movie2);
+// let movie2 = movies.find((movie) => movie.indexOf("Mrs") === 0);
+// console.log(movie2);
 
 /////FILTER///
 //creates a new array with all elements that pass the test implemented by the provided function.
 
-const nums = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-const odds = nums.filter((n) => {
-  return n % 2 === 1;
-});
-console.log(odds);
+// const nums = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// const odds = nums.filter((n) => {
+//   return n % 2 === 1;
+// });
+// console.log(odds);
 
-const smallNums = nums.filter((n) => n <= 5);
-console.log(smallNums);
+// const smallNums = nums.filter((n) => n <= 5);
+// console.log(smallNums);
 
 //using filter with includes
 
-const books = [
-  {
-    title: "Good Omens",
-    authors: ["Terry Pratchett", "Neil Gaiman"],
-    rating: 4.25,
-    genres: ["fiction", "fantasy"],
-  },
-  {
-    title: "Changing My Mind",
-    authors: ["Zadie Smith"],
-    rating: 3.83,
-    genres: ["nonfiction", "essays"],
-  },
-  {
-    title: "Bone: The Complete Edition",
-    authors: ["Jeff Smith"],
-    rating: 4.42,
-    genres: ["fiction", "graphic novel", "fantasy"],
-  },
-  {
-    title: "American Gods",
-    authors: ["Neil Gaiman"],
-    rating: 4.11,
-    genres: ["fiction", "fantasy"],
-  },
-  {
-    title: "A Gentleman in Moscow",
-    authors: ["Amor Towles"],
-    rating: 4.36,
-    genres: ["fiction", "historical fiction"],
-  },
-  {
-    title: "The Name of the Wind",
-    authors: ["Patrick Rothfuss"],
-    rating: 4.54,
-    genres: ["fiction", "fantasy"],
-  },
-  {
-    title: "The Overstory",
-    authors: ["Richard Powers"],
-    rating: 4.19,
-    genres: ["fiction", "short stories"],
-  },
-  {
-    title: "The Way of Kings",
-    authors: ["Brandon Sanderson"],
-    rating: 4.65,
-    genres: ["fantasy", "epic"],
-  },
-  {
-    title: "Lord of the flies",
-    authors: ["William Golding"],
-    rating: 3.67,
-    genres: ["fiction"],
-  },
-];
+// const books = [
+//   {
+//     title: "Good Omens",
+//     authors: ["Terry Pratchett", "Neil Gaiman"],
+//     rating: 4.25,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "Changing My Mind",
+//     authors: ["Zadie Smith"],
+//     rating: 3.83,
+//     genres: ["nonfiction", "essays"],
+//   },
+//   {
+//     title: "Bone: The Complete Edition",
+//     authors: ["Jeff Smith"],
+//     rating: 4.42,
+//     genres: ["fiction", "graphic novel", "fantasy"],
+//   },
+//   {
+//     title: "American Gods",
+//     authors: ["Neil Gaiman"],
+//     rating: 4.11,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "A Gentleman in Moscow",
+//     authors: ["Amor Towles"],
+//     rating: 4.36,
+//     genres: ["fiction", "historical fiction"],
+//   },
+//   {
+//     title: "The Name of the Wind",
+//     authors: ["Patrick Rothfuss"],
+//     rating: 4.54,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "The Overstory",
+//     authors: ["Richard Powers"],
+//     rating: 4.19,
+//     genres: ["fiction", "short stories"],
+//   },
+//   {
+//     title: "The Way of Kings",
+//     authors: ["Brandon Sanderson"],
+//     rating: 4.65,
+//     genres: ["fantasy", "epic"],
+//   },
+//   {
+//     title: "Lord of the flies",
+//     authors: ["William Golding"],
+//     rating: 3.67,
+//     genres: ["fiction"],
+//   },
+// ];
 
-const newArray = books.filter((b) => b.genres.includes("fantasy"));
-console.log(newArray);
+// const newArray = books.filter((b) => b.genres.includes("fantasy"));
+// console.log(newArray);
 
-const short = books.filter(
-  (b) => b.genres.includes("short stories") || b.genres.includes("essays")
-);
-console.log(short);
+// const short = books.filter(
+//   (b) => b.genres.includes("short stories") || b.genres.includes("essays")
+// );
+// console.log(short);
 
-const query = "The";
-const results = books.filter((book) => {
-  const title = book.title.toLocaleLowerCase();
-  return title.includes(query.toLowerCase());
+// const query = "The";
+// const results = books.filter((book) => {
+//   const title = book.title.toLocaleLowerCase();
+//   return title.includes(query.toLowerCase());
+// });
+// console.log(results);
+
+/////EVERY
+//tests whether all elements in the array pass the provided function.
+//it returns a boolean value.  its a test
+
+// const words = ["dog", "dig", "log", "bag", "wag"];
+// const threeLetters = words.every((word) => {
+//   return word.length === 3;
+// });
+// console.log(threeLetters);
+
+// const startWithD = words.every((word) => word[0] === "d");
+// console.log(startWithD);
+
+// const allEndInG = words.every((word) => {
+//   const last = word.length - 1;
+//   return word[last] === "g";
+// });
+// console.log(allEndInG);
+
+//////SOME
+//similar to every, but returns true if ANY of the array elements pass
+//the test function
+
+// const words = ["dog", "jello", "log", "cupcake", "bag", "wag"];
+
+// let someWord = words.some((word) => {
+//   return word.length > 4;
+// });
+// console.log(someWord);
+
+// let someLtr = words.some((word) => word[0] === "z");
+// console.log(someLtr);
+
+// let someCake = words.some((w) => w.includes("cake"));
+// console.log(someCake);
+
+//SORT PT.2  compareFunc(a,b)  THIS WILL MUTATE
+//if compareFunc(a,b) returns less than 0
+//-sort a before b
+
+//if compareFunc(a,b) returns 0
+//-leave a and b unchanged with respect to each other
+
+//if compareFunc(a,b) returns greater than 0
+//-sort b before a
+
+//? using slice() instead of math(),which doesn't work, to create a new array.
+//maybe because .slice makes a new array and doesn't modify or just copy like
+//splice and map?  assuming here.
+
+// const prices = [400.5, 3000, 99.99, 35.99, 12.0, 9500];
+// prices.sort();
+// const ascend = prices.slice().sort((a, b) => a - b); //<=sorts ascending
+// console.log(ascend);
+
+// // const prices = [400.5, 3000, 99.99, 35.99, 12.0, 9500];
+// prices.sort();
+// const descend = prices.slice().sort((a, b) => b - a); //<=sorts descending
+// console.log(descend);
+
+/////Reduce
+//executes a reducer function on each element of the array, resulting in a single value.
+// //summing
+// const myArray = [3, 5, 7, 9, 11].reduce((accumulator, currentValue) => {
+//   return accumulator + currentValue;
+// });
+// console.log(myArray);
+
+// //multiplying
+// const myArray1 = [3, 4, 5, 6, 7].reduce((accumulator, currentValue) => {
+//   return accumulator * currentValue;
+// });
+// console.log(myArray1);
+
+//Find Max Val
+// let grades = [89, 96, 58, 77, 62, 93, 81, 99, 73].reduce((max, currVal) => {
+//   if (currVal > max) return currVal;
+//   return max;
+// });
+// console.log(grades);
+
+let grades = [89, 96, 58, 77, 62, 93, 81, 99, 73].reduce((max, currVal) => {
+  return Math.max(max, currVal);
 });
-console.log(results);
+console.log(grades);
+
+//Starting point
+const sum = [10, 20, 30, 40, 50].reduce((sum, currVal) => {
+  return sum + currVal;
+});
+console.log(sum);
