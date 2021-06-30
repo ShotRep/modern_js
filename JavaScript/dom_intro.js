9f46dc93ddb29887beea3f9a00dbdabd60b86701
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-06-30 12:00:14 -0500

    DOM basics

---
+++ JavaScript/dom_intro.js
@@ -0,0 +1,8 @@
+//DOM INTRO
+//Connect JS with HTML & CSS
+/*
+WHAT IS IT?
+-the dom is a javascript representation of a webpage.
+-it is your javascript 'window' into the contents of a webpage.
+-it is just a bunch of objects that you can interact with via javascript.
+*/
