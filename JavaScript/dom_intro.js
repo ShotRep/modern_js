bd7c358c2c0aca5b048e7d7c23c094fbd0c055a8
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-07-02 08:47:34 -0500

    DOM exercises have been linked to html page

--- JavaScript/dom_intro.js
+++ JavaScript/dom_intro.js
@@ -6,3 +6,16 @@ WHAT IS IT?
 -it is your javascript 'window' into the contents of a webpage.
 -it is just a bunch of objects that you can interact with via javascript.
 */
+
+/*
+Document: the document object is our entry point into the world of the DOM. It contains representations of all the content on a page, plus tons of useful methods and properties
+*/
+
+/*
+Exercises for DOM in separate files below.
+
+getElementById.js
+getElementsByTagName.js
+getElementsByClassName.js
+querySelector_querySelectorAll.js
+*/
